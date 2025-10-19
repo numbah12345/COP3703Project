a0b550d3c406557c6a3b37e4ac8b7973bde836c6
numbah12345 <140943620+numbah12345@users.noreply.github.com>
2025-10-19 17:55:27 -0400

    Add files via upload

---
+++ COP3703Project/src/main/java/unf/g1/project/main.java
@@ -0,0 +1,23 @@
+//This is the main entry point of the project
+
+/*
+Should allow us to:
+    add patient, department, procedures, doctors, medication, and interaction records.
+    Add information about a procedure done on a patient.
+    Add medication/s prescribed to a patient.
+    Given a patient ID, generate their complete health record. The health record should include patient information (name, current address and current phone, primary doctor's name, primary doctor's department), (a) all the procedures they had, (b) all the interactions they had with the hospital (with dates and the details), and (c) all the medications prescribed to them (with dates and details).
+    Given a department name or code find the procedures offered. 
+    Given a doctor's ID list all the procedures they have done.
+ */
+package unf.g1.project;
+
+/**
+ *
+ * @author jakez
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello COP3703!");
+    }
+}
